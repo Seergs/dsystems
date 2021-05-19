954b773ef8616432e30b319ca9da0dc7270711f7
Sergio <S.suarerez@gmail.com>
2021-05-18 23:39:52 -0500

    finish http server

--- actividad6/server.go
+++ actividad6/server.go
@@ -8,6 +8,16 @@ import (
 	"strconv"
 )
 
+const (
+	NEW = "/new"
+	NEW_RESULT = "/newresult"
+	GENERAL_GPA = "/gpa"
+	STUDENT_LIST = "/studentlist"
+	SUBJECT_LIST = "/subjectlist"
+	STUDENT_GPA = "/studentgpa"
+	SUBJECT_GPA = "/subjectgpa"
+)
+
 type AdminGrades struct {
 	subjectGrades map[string]map[string]float64
 	studentGrades map[string]map[string]float64
@@ -25,7 +35,80 @@ func NewAdmin() *AdminGrades {
 	}
 }
 
-var grades = NewAdmin()
+func (g *AdminGrades) addNewGrade(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	fmt.Fprintf(res, render("new.html"))
+}
+
+func (g *AdminGrades) addNewGradeResult(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	if err := req.ParseForm(); err != nil {
+			log.Println(err.Error())
+			fmt.Fprintf(res, render("error.html"), "No se pudo procesar la solicitud, intenta de nuevo más tarde")
+			return
+		}
+		grade, err := strconv.ParseFloat(req.FormValue("grade"), 64)
+		if (err != nil) {
+			fmt.Fprintf(res, render("error.html"), "Algo salió mal al convertir la calificación a número")
+			return
+		}
+		if !g.setStudentGrade(StudentGrade { studentName: req.FormValue("studentName"), subject: req.FormValue("subject"), grade: grade }) {
+			fmt.Fprintf(res, render("error.html"), "Ya existe calificación para este alumno en esta materia, verifica")
+			return
+		}
+		fmt.Fprintf(res, render("added.html"))
+}
+
+func (g *AdminGrades) generalGpa(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	gpa := g.getStudentsGPA()
+	if gpa == 0 {
+		fmt.Fprintf(res, render("error.html"), "Aún no existen calificaciones de alumnos")
+	} else {  
+		fmt.Fprintf(res, render("gpa.html"), "active", "", "", "general es ", gpa)
+	}
+}
+
+func (g *AdminGrades) studentGpa(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	if err := req.ParseForm(); err != nil {
+		log.Println(err.Error())
+		fmt.Fprintf(res, render("error.html"), "No se pudo procesar la solicitud, intenta de nuevo más tarde")
+		return
+	}
+	studentName := req.FormValue("student")
+	if studentName != "" {
+		gpa := g.getStudentGPA(studentName)
+		fmt.Fprintf(res, render("gpa.html"), "", "active", "", " del alumno " + studentName + " es ", gpa)
+		return
+	}
+}
+
+func (g *AdminGrades) subjectGpa(res http.ResponseWriter, req *http.Request){
+	log.Println("Request made to " + req.RequestURI)
+	if err := req.ParseForm(); err != nil {
+		log.Println(err.Error())
+		fmt.Fprintf(res, render("error.html"), "No se pudo procesar la solicitud, intenta de nuevo más tarde")
+		return
+	}
+	subjectName := req.FormValue("subject")
+	if subjectName != "" {
+		gpa := g.getSubjectGPA(subjectName)
+		fmt.Fprintf(res, render("gpa.html"), "", "", "active", " en " + subjectName + " es ", gpa)
+		return
+	}
+}
+
+func (g *AdminGrades) studentList(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	fmt.Fprintf(res, render("student-list.html"), g.studentsToHtml())
+}
+
+func (g *AdminGrades) subjectList(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request made to " + req.RequestURI)
+	fmt.Fprintf(res, render("subject-list.html"), g.subjectsToHtml())
+}
+
 
 func (g *AdminGrades) setStudentGrade(studentGrade StudentGrade) bool {
 	log.Printf("Attempting to set grade %f on subject %s for student %s", studentGrade.grade, studentGrade.subject, studentGrade.studentName)
@@ -131,7 +214,6 @@ func (g *AdminGrades) studentsToHtml() string {
 
 func (g *AdminGrades) subjectsToHtml() string {
 	subjects := g.getSubjectNames()
-	log.Println(subjects)
 	var html string
 	for _,subject := range subjects {
 		html += "<option value='" + subject + "'>" +
@@ -149,66 +231,17 @@ func calculateGPA(grades []float64) float64 {
 }
 
 func main() {
-	http.HandleFunc("/new", newGradeCrontoller)
-	http.HandleFunc("/gpa", gpaController)
+	admin := NewAdmin()
+	http.HandleFunc(NEW, admin.addNewGrade)
+	http.HandleFunc(NEW_RESULT, admin.addNewGradeResult)
+	http.HandleFunc(STUDENT_LIST, admin.studentList)
+	http.HandleFunc(SUBJECT_LIST, admin.subjectList)
+	http.HandleFunc(GENERAL_GPA, admin.generalGpa)
+	http.HandleFunc(STUDENT_GPA, admin.studentGpa)
+	http.HandleFunc(SUBJECT_GPA, admin.subjectGpa)
 	log.Println("Server started on port 5000")
 	http.ListenAndServe(":5000", nil)
 }
-
-func newGradeCrontoller(res http.ResponseWriter, req *http.Request) {
-	log.Printf("New request received to /new with method, %s", req.Method )
-	switch(req.Method) {
-	case "GET":
-		fmt.Fprintf(res, render("new.html"))
-		break
-	case "POST":
-		if err := req.ParseForm(); err != nil {
-			log.Println(err.Error())
-			fmt.Fprintf(res, render("error.html"), "No se pudo procesar la solicitud, intenta de nuevo más tarde")
-			return
-		}
-		grade, err := strconv.ParseFloat(req.FormValue("grade"), 64)
-		if (err != nil) {
-			fmt.Fprintf(res, render("error.html"), "Algo salió mal al convertir la calificación a número")
-			return
-		}
-		if !grades.setStudentGrade(StudentGrade { studentName: req.FormValue("studentName"), subject: req.FormValue("subject"), grade: grade }) {
-			fmt.Fprintf(res, render("error.html"), "Ya existe calificación para este alumno en esta materia, verifica")
-			return
-		}
-		fmt.Fprintf(res, render("added.html"))
-		break
-	default:
-		fmt.Fprintf(res, render("error.html"), "No se pudo procesar la solicitud, intenta de nuevo más tarde")
-	}
-}
-
-func gpaController(res http.ResponseWriter, req *http.Request) {
-	log.Printf("New request received to /gpa with method, %s", req.Method )
-	gpaType := req.URL.Query().Get("type")
-
-	switch gpaType {
-	case "":
-		gpa := grades.getStudentsGPA()
-		if gpa == 0 {
-			fmt.Fprintf(res, render("error.html"), "Aún no existen calificaciones de alumnos")
-		}else{ 
-			fmt.Fprintf(res, render("gpa.html"), "general", grades.getStudentsGPA())
-		}
-		break
-	case "student":
-		if req.Method == "GET"{
-			fmt.Fprintf(res, render("list.html"), grades.studentsToHtml())
-		}
-			
-		break
-	case "subject":
-		if req.Method == "GET" {
-			fmt.Fprintf(res, render("list.html"), grades.subjectsToHtml())
-		}
-	}
-}
-
 
 func render(path string) string {
 	html, _ := ioutil.ReadFile(path)
